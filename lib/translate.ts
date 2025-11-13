e518ec3eb5d8676b31b9026594d050928a7983c8
iddi <iddi5504@gmail.com>
2025-11-13 13:58:47 +0000

    return null if translatoin fails

--- lib/translate.ts
+++ lib/translate.ts
@@ -1,6 +1,7 @@
 import { TranslationResponse } from "@/types/global";
 
-export const translate = async (key: string, body: string): Promise<TranslationResponse> => {
+export const translate = async (key: string, body: string): Promise<TranslationResponse | null> => {
+
     const response = await fetch("https://api.lecto.ai/v1/translate/text", {
         method: "POST",
         body: JSON.stringify({
@@ -10,5 +11,10 @@ export const translate = async (key: string, body: string): Promise<TranslationR
         }),
         headers: { "Content-Type": "application/json", "x-api-key": process.env.NEXT_PUBLIC_LECTO_API_KEY || "" },
     });
-    return response.json();
+
+    if (response.ok === false) {
+        return null;
+    }
+
+    return await response.json();
 }
